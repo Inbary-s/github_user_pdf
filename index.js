9c515635c3e7d1d390f9c7e5fa70ba1f5d2099f3
inbary-s <inbarsim6@gmail.com>
2020-01-10 20:23:50 -0700

    working code

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 // Require Axios
 const axios = require("axios");
+// Require HTML/PDF
+const pdf = require("html-pdf");
+const awesome = require("@fortawesome/fontawesome-free")
 
 createPage = (data, color) =>{
 return (`<!DOCTYPE html>
@@ -13,14 +16,87 @@ return (`<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel = "stylesheet" href = "assets/reset.css">  
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel = "stylesheet" href = "node_modules/@fortawesome/fontawesome-free/css/all.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
     <link href="https://fonts.googleapis.com/css?family=Darker+Grotesque&display=swap" rel="stylesheet">
-    <link rel = "stylesheet" href = "assets/style.css">  
-<title>GitHub User Profile PDF</title>
+  
+    <title>GitHub User Profile PDF</title>
+    <style>
+    
+    body{
+        font-family: 'Darker Grotesque', sans-serif;
+        align-content: center;
+        color: white;
+    }
+    .avatar img{
+        border-radius: 100px;
+        width: 200px;
+        height: 200px;
+        border: solid 12px rgb(255, 255, 255);
+        -webkit-box-shadow: -12px 12px 12px 0px rgba(255,255,255,0.36), -6px 6px 6px 0px rgba(255, 255, 255, 0.603);
+    -moz-box-shadow: -12px 12px 12px 0px rgba(255,255,255,0.36), -6px 6px 6px 0px rgba(255, 255, 255, 0.603);
+    box-shadow: -12px 12px 12px 0px rgba(255,255,255,0.36), -6px 6px 6px 0px rgba(255, 255, 255, 0.603);
+        position: absolute;
+        z-index: 2;
+        left: 20%;
+    }
+    .container {
+        /* background-color: rgba(255, 255, 255, 0.5); */
+        /* padding: 50px; */
+        margin-top: 50px;
+        border-radius: 15px;
+        /* position: relative; */
+        top: 100px;
+        z-index: 0;
+    }
+    .textContainer {
+        background-color: rgba(255, 255, 255, 0.5);
+        padding: 40px;
+        /* margin-bottom: 20px; */
+        border-radius: 15px;
+        position: relative;
+        top: 100px;
+        z-index: 0;
+    }
+    .cardContainer {
+        position: relative;
+        top: 100px;
+        padding: 10px;
+        margin: 20px;
+        align-items: center;
+        width: 100%;
+    }
+    .card{
+        background: linear-gradient(0deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 90%);
+        margin: 20px;
+        padding: 40px;
+        height: 150px;
+        width: 200px;
+        border-radius: 15px;
+        border: none;
+    }
+    a {
+        color: white;
+        margin: 5%;
+    }
+    i{
+        padding-right: 5px;
+    }
+    h2 {
+        font-weight: 600;
+        padding-top: 50px;
+    }
+    h4{
+        font-weight: 600;
+        color: currentColor;
+    }
+    span{width: 100%;
+        text-align: center;}
+        
+    </style>
 </head>
 <body style="background-color:${color}">
     <div class="container justify-content-center">
@@ -82,22 +158,43 @@ inquirer.prompt([
 ]).then(function({ githubUn, color }) {
     const queryUrl = `https://api.github.com/users/${githubUn}`
     axios.get(queryUrl).then(function(res) {
-        function white(){
-            if (color === "white"){
-                color = "black"
+        // function white(){
+        //     if (color === "white"){
+        //         color = "black"
+        //     }
+        // };
+        // white();
+        fs.writeFile(res.data.login+".html", createPage(res.data, (color==="white")? "black": color), function(err) {
+            console.log("write", res.data.login+".html")
+            if (err) {
+            throw err;
             }
-        };
-        white();
-            fs.writeFile(res.data.login+".html", createPage(res.data, color), function(err) {
-                if (err) {
-                throw err;
-                }
-            console.log(res.data);
-            console.log(queryUrl);
-            return res.data;
-        })
-    });
+        console.log(res.data);
+        console.log(queryUrl);
+        const options = { format: 'Letter', timeout: "100000" };
+    const filename = "./" + res.data.login;
+    console.log("read", filename)
+    const html = fs.readFileSync(filename + ".html", 'utf8');
+    console.log(html)
+setTimeout(()=>{pdf.create(html, options).toFile(filename+'.pdf', function(err, res) {
+  if (err) return console.log(err);
+  console.log(res); // { filename: '/app/businesscard.pdf' }
+});
+        return res.data;
+    }, 4000)
 })
+});
+});
+
+// function print(file) {
+//     const filename  = 'testOutput.pdf';
+
+//     html2canvas(file).then(canvas => {
+//         let pdf = new jsPDF('p', 'mm', 'a4');
+//         pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
+//         pdf.save(filename);
+//     });
+// }
 
 
 
